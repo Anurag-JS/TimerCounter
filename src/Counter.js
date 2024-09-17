d482817f571e4a9a0f4debacfc8e78998ab10e92
Anurag <anuragv8269@gmail.com>
2024-09-17 17:15:28 +0530

    Created Counter Component

---
+++ src/Counter.js
@@ -0,0 +1,13 @@
+
+
+export default function Counter (){
+
+    return(
+        <div>
+            {}
+            <button>Pause</button>
+            <button>Resume</button>
+            <button>Reset</button>
+        </div>
+    )
+}
