65ee047541f497e44e8c63d34a43b8e048c0a0cf
Anurag <anuragv8269@gmail.com>
2024-09-17 17:23:21 +0530

    Implemented Timer functionality

--- src/Counter.js
+++ src/Counter.js
@@ -1,6 +1,21 @@
-
+import { useEffect, useState } from "react";
 
 export default function Counter (){
+
+    const [timer,setTimer] = useState(0);
+    const [paused,setPaused] = useState(false);
+
+    useEffect(()=>{
+        const timerId = setInterval(()=>{
+            if(!paused){
+                setTimer((prev) => prev + 1);
+            }
+        },1000)
+    },[])
+
+    const handlePause = ()=>{
+        setPaused(true);
+    }
 
     return(
         <div>
